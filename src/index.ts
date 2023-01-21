6bc6aa6ba3b4e0bd31ff352de613ad1fa0990cbf
Tillmann Rendel <tillmann.rendel@posteo.de>
2023-01-21 23:53:59 +0000

    Generate SVG

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,16 @@
-import chalk from "chalk" 
+import chalk from "chalk";
+import { SVG, registerWindow } from "@svgdotjs/svg.js";
+import { createSVGWindow } from 'svgdom';
 
+// pretend we are in a browser
+const window = createSVGWindow()
+const document = window.document
+registerWindow(window, document)
+
+// draw a picture
+const picture = SVG()
+picture.rect(100, 100).fill('yellow').move(50,50)
+
+// show the world
 console.log(chalk.red("Hello") + " " + chalk.green("World"));
+console.log(picture.svg());
